425b058b71b55dc7fca4c93fe7f337bdb71743e3
Avinashkrmehta <avi4791@gmail.com>
2024-06-28 04:20:30 +0530

    added gemfiles and sum calculation logic

---
+++ lib/string_calculator.rb
@@ -0,0 +1,18 @@
+class StringCalculator
+  def add(numbers)
+    return 0 if numbers.empty?
+
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      parts = numbers.split("\n", 2)
+      delimiter = Regexp.escape(parts[0][2])
+      numbers = parts[1]
+    end
+
+    num_list = numbers.split(/#{delimiter}/).map(&:to_i)
+    negatives = num_list.select { |num| num < 0 }
+    raise "Negative numbers not allowed: #{negatives.join(', ')}" if negatives.any?
+
+    num_list.sum
+  end
+end
